d50ee482bb382578fd008036b9ceb7792e93eb4b
mikanikos <andrea.piccione@epfl.ch>
2020-02-25 21:53:23 +0100

    Add greeting message upon button pressing and update Readme and .gitignore

--- app/src/androidTest/java/ch/epfl/favo/MainActivityTest.java
+++ app/src/androidTest/java/ch/epfl/favo/MainActivityTest.java
@@ -11,7 +11,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
@@ -22,6 +24,6 @@ public class MainActivityTest {
     public void testCanGreetUsers() {
         onView(withId(R.id.mainName)).perform(typeText("from my unit test")).perform(closeSoftKeyboard());
         onView(withId(R.id.mainGoButton)).perform(click());
-        //onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
+        onView(withId(R.id.greetingMessage)).check(matches(withText("Hello from my unit test!")));
     }
 }
