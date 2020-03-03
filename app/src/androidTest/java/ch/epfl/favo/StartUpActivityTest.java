42ee18e18329602525790470f734556b82ce1eea
danielpgp1012 <daniel.pachecogutierrez@epfl.ch>
2020-03-03 10:37:27 +0100

    Update Intent Checker in Startup Test

--- app/src/androidTest/java/ch/epfl/favo/StartUpActivityTest.java
+++ app/src/androidTest/java/ch/epfl/favo/StartUpActivityTest.java
@@ -1,9 +1,19 @@
 package ch.epfl.favo;
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
+
+import ch.epfl.favo.presenter.LoginActivity;
+import ch.epfl.favo.presenter.MainActivity;
 import ch.epfl.favo.presenter.StartupActivity;
 
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.intent.rule.IntentsTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -16,6 +26,9 @@ import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.toPackage;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
@@ -23,19 +36,30 @@ import static androidx.test.espresso.matcher.ViewMatchers.withText;
 public class StartUpActivityTest {
 
     @Rule
-    public ActivityTestRule<StartupActivity> mActivity =
-           new ActivityTestRule<>(StartupActivity.class);
+    public ActivityTestRule<StartupActivity> activityRule =
+            new ActivityTestRule<>(StartupActivity.class);
+
+    @Before
+    public void setUp() {
+        Intents.init();
+    }
 
     @Test
     public void LoginShouldTakeToMainActivity(){
-
-        mActivity.getActivityResult();
-        //login successfully.
+        activityRule.launchActivity(new Intent());
+        //Check login screen is launched
+        intended(hasComponent(LoginActivity.class.getName()));
+        //Log in
         onView(withId(R.id.login_button)).perform(click());
-        //Check main activity is launched.
-        onView(withId(R.id.text1)).check(matches(withText("1")));
+        //Check main screen is launched
+        intended(hasComponent(MainActivity.class.getName()));
 
 
+
+    }
+    @After
+    public void cleanUp(){
+        Intents.release();
     }
 
 }
