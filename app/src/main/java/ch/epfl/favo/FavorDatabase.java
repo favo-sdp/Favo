78fc2a2bc6112821b9ac443a60e8daa85e829503
muneebazam <mmazam@edu.uwaterloo.ca>
2020-03-05 10:29:45 +0100

    Create initial database/backend skeleton

--- app/src/main/java/ch/epfl/favo/FavorDatabase.java
+++ app/src/main/java/ch/epfl/favo/FavorDatabase.java
@@ -1,22 +1,8 @@
 package ch.epfl.favo;
 
-import android.provider.ContactsContract;
-
-import java.util.ArrayList;
-
 public final class FavorDatabase {
 
     public FavorDatabase() {}
 
-    public static String addFavorToDatabase(ArrayList favorDetails) {
-        DatabaseWrapper.getDatabaseInstance();
-        return "";
-    }
-
-    public static boolean deleteFavorFromDatabase(String favorId) {
-        boolean success = false;
-        DatabaseWrapper.getDatabaseInstance();
-        return success;
-    }
 
 }
