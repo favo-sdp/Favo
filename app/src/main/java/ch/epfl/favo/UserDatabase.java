c76c09ffd426c004a8adeff4b1bab8a4a8b466bf
muneebazam <mmazam@edu.uwaterloo.ca>
2020-03-05 10:29:45 +0100

    Create initial database/backend skeleton

--- app/src/main/java/ch/epfl/favo/UserDatabase.java
+++ app/src/main/java/ch/epfl/favo/UserDatabase.java
@@ -1,20 +1,7 @@
 package ch.epfl.favo;
 
-import java.util.ArrayList;
-
 public final class UserDatabase {
 
     public UserDatabase() {}
-
-    public static String addUserToDatabase(ArrayList userDetails) {
-        DatabaseWrapper.getDatabaseInstance();
-        return "";
-    }
-
-    public static boolean deleteUserFromDatabase(String userId) {
-        boolean success = false;
-        DatabaseWrapper.getDatabaseInstance();
-        return success;
-    }
 
 }
