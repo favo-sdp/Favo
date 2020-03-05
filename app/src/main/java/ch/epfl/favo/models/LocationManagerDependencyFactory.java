b4866420319ce4aba4bd1fb317b269cf9b377b8d
huajian1069 <huajian1069@gmail.com>
2020-03-05 15:27:07 +0100

    add missing file

---
+++ app/src/main/java/ch/epfl/favo/models/LocationManagerDependencyFactory.java
@@ -0,0 +1,43 @@
+package ch.epfl.favo.models;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class LocationManagerDependencyFactory  extends Service  {
+
+    private static boolean variableSet = false;
+    private static Context context;
+    private static LocationManager currentLocationManager;
+
+    public  LocationManagerDependencyFactory(Context context){
+        this.context = context;
+        currentLocationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
+    }
+
+    public static LocationManager getCurrentLocationManager() {
+        if (variableSet) {
+            return currentLocationManager;
+        }
+        return (LocationManager) context.getSystemService(LOCATION_SERVICE);
+    }
+
+    public static void setCurrentLocationManager(LocationManager dependency) {
+        variableSet = true;
+        LocationManagerDependencyFactory.currentLocationManager = dependency;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
