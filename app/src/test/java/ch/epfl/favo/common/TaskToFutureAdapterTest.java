6023d6c38ca443c1acb99b7bf9b81d16c857cef5
Alex <alexrosu97@gmail.com>
2020-03-24 10:28:48 +0100

    testing TaskToAdapter

--- app/src/test/java/ch/epfl/favo/common/TaskToFutureAdapterTest.java
+++ app/src/test/java/ch/epfl/favo/common/TaskToFutureAdapterTest.java
@@ -1,97 +1,48 @@
 package ch.epfl.favo.common;
 
-import android.app.Activity;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
 import org.junit.Test;
 
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutionException;
+
+import ch.epfl.favo.util.TestTask;
+
+import static junit.framework.TestCase.assertEquals;
 
 public class TaskToFutureAdapterTest {
 
   @Test
-  public void addTwoNumbersTask() {
-    Task<Void> task = new Task<Void>() {
-      @Override
-      public boolean isComplete() {
-        return false;
-      }
+  public void taskToFutureAdapterSuccess() throws ExecutionException, InterruptedException {
+    TestTask task = new TestTask();
+    CompletableFuture<String> f = new TaskToFutureAdapter<>(task);
+    task.complete();
+    // check that the listener is triggered properly (MAY BE LATER)
+    f.thenApply(
+        value -> {
+          assertEquals(task.value, value);
+          return value;
+        });
+    // explicitly wait because we want the test to actually check the future termination
+    String result = f.get();
+    assertEquals(task.value, result);
+  }
 
-      @Override
-      public boolean isSuccessful() {
-        return false;
-      }
-
-      @Override
-      public boolean isCanceled() {
-        return false;
-      }
-
-      @Nullable
-      @Override
-      public Void getResult() {
-        return null;
-      }
-
-      @Nullable
-      @Override
-      public <X extends Throwable> Void getResult(@NonNull Class<X> aClass) throws X {
-        return null;
-      }
-
-      @Nullable
-      @Override
-      public Exception getException() {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnSuccessListener(@NonNull OnSuccessListener<? super Void> onSuccessListener) {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnSuccessListener(@NonNull Executor executor, @NonNull OnSuccessListener<? super Void> onSuccessListener) {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnSuccessListener(@NonNull Activity activity, @NonNull OnSuccessListener<? super Void> onSuccessListener) {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnFailureListener(@NonNull OnFailureListener onFailureListener) {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnFailureListener(@NonNull Executor executor, @NonNull OnFailureListener onFailureListener) {
-        return null;
-      }
-
-      @NonNull
-      @Override
-      public Task<Void> addOnFailureListener(@NonNull Activity activity, @NonNull OnFailureListener onFailureListener) {
-        return null;
-      }
-    };
-
-    CompletableFuture<Void> future = new TaskToFutureAdapter<Void>(task);
-    future.complete(null);
-    assert(true);
-
+  @Test
+  public void taskToFutureAdapterFailure() throws InterruptedException {
+    TestTask task = new TestTask();
+    CompletableFuture<String> f = new TaskToFutureAdapter<>(task);
+    task.fail();
+    // check that listener is triggered properly (MAY BE LATER)
+    f.exceptionally(
+        exception -> {
+          assertEquals(task.exception, exception);
+          return null;
+        });
+    // explicitly wait because we want the test to actually check the future termination
+    try {
+      f.get();
+    } catch (ExecutionException e) {
+      assertEquals(task.exception, e.getCause());
+    }
   }
 }
---
+++ app/src/test/java/ch/epfl/favo/util/TestTask.java
@@ -0,0 +1,137 @@
+package ch.epfl.favo.util;
+
+import android.app.Activity;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executor;
+
+public class TestTask extends Task<String> {
+  // possible task status
+  enum TaskStatus {
+    PENDING,
+    SUCCESS,
+    FAILURE,
+    CANCELLED
+  }
+
+  // success listeners
+  private final List<OnSuccessListener> successes = new ArrayList<>();
+  // failure listeners
+  private final List<OnFailureListener> failures = new ArrayList<>();
+  // task status
+  private TaskStatus status = TaskStatus.PENDING;
+  // value of the successful task, constant for testing
+  public final String value = "Hello world!";
+  // exception to throw if task is not successful
+  public final RuntimeException exception =
+      new RuntimeException("Task was not finished successfully");
+
+  @Override
+  public boolean isComplete() {
+    return status != TaskStatus.PENDING;
+  }
+
+  @Override
+  public boolean isSuccessful() {
+    return status == TaskStatus.SUCCESS;
+  }
+
+  @Override
+  public boolean isCanceled() {
+    return status == TaskStatus.CANCELLED;
+  }
+
+  @Nullable
+  @Override
+  public String getResult() {
+    if (status != TaskStatus.SUCCESS) {
+      throw exception;
+    }
+    return value;
+  }
+
+  @Nullable
+  @Override
+  public <X extends Throwable> String getResult(@NonNull Class<X> aClass) throws X {
+    if (status != TaskStatus.SUCCESS) {
+      throw exception;
+    }
+    return value;
+  }
+
+  @Nullable
+  @Override
+  public Exception getException() {
+    if (status != TaskStatus.FAILURE) {
+      throw new RuntimeException("Task did not fail");
+    }
+    return exception;
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnSuccessListener(
+      @NonNull OnSuccessListener<? super String> onSuccessListener) {
+    successes.add(onSuccessListener);
+    return this;
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnSuccessListener(
+      @NonNull Executor executor, @NonNull OnSuccessListener<? super String> onSuccessListener) {
+    return addOnSuccessListener(onSuccessListener);
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnSuccessListener(
+      @NonNull Activity activity, @NonNull OnSuccessListener<? super String> onSuccessListener) {
+    return addOnSuccessListener(onSuccessListener);
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnFailureListener(@NonNull OnFailureListener onFailureListener) {
+    failures.add(onFailureListener);
+    return this;
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnFailureListener(
+      @NonNull Executor executor, @NonNull OnFailureListener onFailureListener) {
+    return addOnFailureListener(onFailureListener);
+  }
+
+  @NonNull
+  @Override
+  public Task<String> addOnFailureListener(
+      @NonNull Activity activity, @NonNull OnFailureListener onFailureListener) {
+    return addOnFailureListener(onFailureListener);
+  }
+
+  // force the success of the task
+  public void complete() {
+    status = TaskStatus.SUCCESS;
+    for (final OnSuccessListener success : successes) {
+      success.onSuccess(value);
+    }
+  }
+
+  // force failure of the task
+  public void fail() {
+    status = TaskStatus.FAILURE;
+    for (final OnFailureListener failure : failures) {
+      failure.onFailure(exception);
+    }
+  }
+}
