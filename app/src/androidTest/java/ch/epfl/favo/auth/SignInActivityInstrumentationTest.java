6d3dd39a9b514616ce1b27aff602ab83472866c2
mikanikos <andrea.piccione@epfl.ch>
2020-03-26 20:13:12 +0100

    Try to fix facebook test on Cirrus

--- app/src/androidTest/java/ch/epfl/favo/auth/SignInActivityInstrumentationTest.java
+++ app/src/androidTest/java/ch/epfl/favo/auth/SignInActivityInstrumentationTest.java
@@ -1,11 +1,11 @@
 package ch.epfl.favo.auth;
 
+import android.os.RemoteException;
 import android.widget.EditText;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-import androidx.test.rule.GrantPermissionRule;
 import androidx.test.uiautomator.By;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.UiObject;
@@ -13,38 +13,36 @@ import androidx.test.uiautomator.UiObject2;
 import androidx.test.uiautomator.UiObjectNotFoundException;
 import androidx.test.uiautomator.UiSelector;
 
-import com.firebase.ui.auth.IdpResponse;
-
 import org.junit.After;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ch.epfl.favo.FakeFirebaseUser;
-import ch.epfl.favo.R;
 import ch.epfl.favo.util.DependencyFactory;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
-import static ch.epfl.favo.TestConstants.EMAIL;
-import static ch.epfl.favo.TestConstants.NAME;
-import static ch.epfl.favo.TestConstants.PHOTO_URI;
-import static ch.epfl.favo.TestConstants.PROVIDER;
-import static org.hamcrest.Matchers.anyOf;
-import static org.hamcrest.core.StringEndsWith.endsWith;
 
 @RunWith(AndroidJUnit4.class)
-public class SignInActivityTest {
+public class SignInActivityInstrumentationTest {
 
   @Rule
   public final ActivityTestRule<SignInActivity> mActivityRule =
       new ActivityTestRule<>(SignInActivity.class);
+
+  @Before
+  public void setup() {
+    DependencyFactory.setCurrentFirebaseUser(null);
+  }
+
+  @After
+  public void tearDown() {
+    DependencyFactory.setCurrentFirebaseUser(null);
+  }
 
   @Test
   public void testGenerateNotificationToken() {
@@ -52,9 +50,12 @@ public class SignInActivityTest {
   }
 
   @Test
-  public void testFacebookLogin() throws UiObjectNotFoundException, InterruptedException {
+  public void testFacebookLogin()
+      throws UiObjectNotFoundException, InterruptedException, RemoteException {
 
     UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+    mDevice.wakeUp();
 
     // wait to be in the login page
     while (!mDevice.findObject(new UiSelector().textContains("Sign in with Facebook")).exists()) {
@@ -67,19 +68,18 @@ public class SignInActivityTest {
 
     Thread.sleep(5000);
 
-    // insert email
-    UiObject2 editText = mDevice.findObject(By.clazz("android.widget.EditText"));
-    editText.setText("vhnwqbiihe_1583436753@tfbnw.net");
-
-    // insert password
-    UiObject input = mDevice.findObject(new UiSelector()
-            .instance(1)
-            .className(EditText.class));
-    input.setText("favo123");
-
     // click on login
-    if (mDevice.findObject(new UiSelector().clickable(true).textContains("Log In")).exists())
+    if (mDevice.findObject(new UiSelector().clickable(true).textContains("Log In")).exists()) {
+      // insert email
+      UiObject2 editText = mDevice.findObject(By.clazz("android.widget.EditText"));
+      editText.setText("vhnwqbiihe_1583436753@tfbnw.net");
+
+      // insert password
+      UiObject input = mDevice.findObject(new UiSelector().instance(1).className(EditText.class));
+      input.setText("favo123");
+
       mDevice.findObject(new UiSelector().clickable(true).textContains("Log In")).click();
+    }
 
     Thread.sleep(3000);
 
@@ -89,5 +89,4 @@ public class SignInActivityTest {
 
     Thread.sleep(3000);
   }
-
 }
