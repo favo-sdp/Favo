7e1a534aabf43cc966bc0acd19942e5dd8b07940
mikanikos <andrea.piccione@epfl.ch>
2020-03-26 19:38:41 +0100

    Add proper test for facebook login flow

--- app/src/androidTest/java/ch/epfl/favo/auth/SignInActivityTest.java
+++ app/src/androidTest/java/ch/epfl/favo/auth/SignInActivityTest.java
@@ -1,8 +1,17 @@
 package ch.epfl.favo.auth;
 
+import android.widget.EditText;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiDevice;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObject2;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 
 import com.firebase.ui.auth.IdpResponse;
 
@@ -41,4 +50,44 @@ public class SignInActivityTest {
   public void testGenerateNotificationToken() {
     mActivityRule.getActivity().retrieveCurrentRegistrationToken();
   }
+
+  @Test
+  public void testFacebookLogin() throws UiObjectNotFoundException, InterruptedException {
+
+    UiDevice mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+
+    // wait to be in the login page
+    while (!mDevice.findObject(new UiSelector().textContains("Sign in with Facebook")).exists()) {
+      Thread.sleep(3000);
+    }
+
+    // click on sign in with facebook
+    onView(withText("Sign in with Facebook")).check(matches(isDisplayed()));
+    onView(withText("Sign in with Facebook")).perform(click());
+
+    Thread.sleep(5000);
+
+    // insert email
+    UiObject2 editText = mDevice.findObject(By.clazz("android.widget.EditText"));
+    editText.setText("vhnwqbiihe_1583436753@tfbnw.net");
+
+    // insert password
+    UiObject input = mDevice.findObject(new UiSelector()
+            .instance(1)
+            .className(EditText.class));
+    input.setText("favo123");
+
+    // click on login
+    if (mDevice.findObject(new UiSelector().clickable(true).textContains("Log In")).exists())
+      mDevice.findObject(new UiSelector().clickable(true).textContains("Log In")).click();
+
+    Thread.sleep(3000);
+
+    // click on continue if present (when already logged in)
+    if (mDevice.findObject(new UiSelector().clickable(true).textContains("Continue")).exists())
+      mDevice.findObject(new UiSelector().clickable(true).textContains("Continue")).click();
+
+    Thread.sleep(3000);
+  }
+
 }
