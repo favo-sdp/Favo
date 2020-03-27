c0afec42479088066820fd034dc2739c06d8308e
huajian1069 <huajian1069@gmail.com>
2020-03-28 00:47:08 +0100

    no risk to pass curris

--- app/src/main/java/ch/epfl/favo/view/tabs/MapsPage.java
+++ app/src/main/java/ch/epfl/favo/view/tabs/MapsPage.java
@@ -1,5 +1,6 @@
 package ch.epfl.favo.view.tabs;
 
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.location.Location;
 import android.os.Bundle;
@@ -12,8 +13,14 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -22,6 +29,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +58,10 @@ public class MapsPage extends Fragment
   private Location mLocation;
   private GpsTracker mGpsTracker;
   private ArrayList<Favor> currentActiveLocalFavorList = null;
+  private boolean first=true;
+  private boolean mLocationPermissionGranted = false;
+  private FusedLocationProviderClient mFusedLocationProviderClient;
+  private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
 
   public MapsPage() {
     // Required empty public constructor
@@ -89,6 +102,69 @@ public class MapsPage extends Fragment
     }
   }
 
+  private void checkPlayServices() {
+    GoogleApiAvailability gApi = GoogleApiAvailability.getInstance();
+    int resultCode = gApi.isGooglePlayServicesAvailable(getActivity());
+    if (resultCode != ConnectionResult.SUCCESS) {
+      gApi.makeGooglePlayServicesAvailable(getActivity());
+    }
+  }
+
+  private void getLocationPermission() {
+    /*
+     * Request location permission, so that we can get the location of the
+     * device. The result of the permission request is handled by a callback,
+     * onRequestPermissionsResult.
+     */
+    if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getActivity()).getApplicationContext(),
+            android.Manifest.permission.ACCESS_FINE_LOCATION)
+            == PackageManager.PERMISSION_GRANTED) {
+      mLocationPermissionGranted = true;
+    } else {
+      ActivityCompat.requestPermissions(Objects.requireNonNull(getActivity()),
+              new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+              PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+    }
+  }
+
+  public void getLocation() throws NoPermissionGrantedException, NoPositionFoundException {
+    getLocationPermission();
+    if (mLocationPermissionGranted) {
+      LocationRequest request = new LocationRequest();
+      request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+      request.setInterval(15 * 60 * 1000);
+      request.setMaxWaitTime(30 * 60 * 1000);
+      Task<Location> locationResult = mFusedLocationProviderClient.getLastLocation();
+      locationResult.addOnCompleteListener(Objects.requireNonNull(getActivity()), new OnCompleteListener<Location>() {
+        @Override
+        public void onComplete(@NonNull Task<Location> task) {
+          if (task.isSuccessful()) {
+            // Set the map's camera position to the current location of the device.
+            mLocation = task.getResult();
+            GpsTracker.setLastKnownLocation(mLocation);
+          }
+        }
+      });
+    }
+  }
+
+  /**
+   * Handles the result of the request for location permissions.
+   */
+  @Override
+  public void onRequestPermissionsResult(int requestCode,
+                                         @NonNull String[] permissions,
+                                         @NonNull int[] grantResults) {
+    mLocationPermissionGranted = false;
+    if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
+      if (grantResults.length > 0
+              && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        mLocationPermissionGranted = true;
+      }
+    }
+  }
+
+
   public List<Favor> updateFavorlist() {
     // FavorUtil favorUtil = FavorUtil.getSingleInstance();
     // return favorUtil.retrieveAllFavorsInGivenRadius(mLocation, 2);
@@ -104,7 +180,7 @@ public class MapsPage extends Fragment
     }
   }
 
-  public void drawSelfLocationMarker() {
+  private void drawSelfLocationMarker() {
     try {
       mLocation = mGpsTracker.getLocation();
       // Add a marker at my location and move the camera
