4ab3ae8a1134e3d6dcda0e352361be540cbe8770
muneebazam <mmazam@edu.uwaterloo.ca>
2020-04-02 21:22:02 -0400

    Change mock database to match real

--- app/src/sharedTestUtils/java/ch/epfl/favo/view/MockDatabaseWrapper.java
+++ app/src/sharedTestUtils/java/ch/epfl/favo/view/MockDatabaseWrapper.java
@@ -2,20 +2,20 @@ package ch.epfl.favo.view;
 
 import android.location.Location;
 
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import ch.epfl.favo.FakeItemFactory;
 import ch.epfl.favo.common.DatabaseUpdater;
+import ch.epfl.favo.common.Document;
 import ch.epfl.favo.favor.Favor;
 
-public class MockDatabaseWrapper implements DatabaseUpdater<Favor> {
+public class MockDatabaseWrapper<T extends Document> implements DatabaseUpdater<T> {
   @Override
-  public void addDocument(Favor favor) {}
+  public void addDocument(T favor) {}
 
   @Override
-  public void updateDocument(Favor document) {
-
-  }
+  public void updateDocument(String key, Map<String, Object> updates) {}
 
   @Override
   public void removeDocument(String key) {
