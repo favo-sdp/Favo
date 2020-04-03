c9bf1328e298ca0907055d63b0884db374e142ef
Alex <alexrosu97@gmail.com>
2020-04-03 12:10:54 -0400

    FavoLocation test for increasing coverage

---
+++ app/src/test/java/ch/epfl/favo/common/FavoLocationTest.java
@@ -0,0 +1,30 @@
+package ch.epfl.favo.common;
+
+import android.location.Location;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class FavoLocationTest {
+
+  private static final String PROVIDER = "PROVIDER";
+
+  @Test
+  public void defaultNoArgConstructor() {
+    FavoLocation location = new FavoLocation();
+    assertEquals(null, location.getProvider());
+  }
+
+  @Test
+  public void providerConstructor() {
+    FavoLocation location = new FavoLocation(PROVIDER);
+    assertEquals(null, location.getProvider());
+  }
+
+  @Test
+  public void locationConstructor() {
+    FavoLocation location = new FavoLocation(new Location(PROVIDER));
+    assertEquals(null, location.getProvider());
+  }
+}
