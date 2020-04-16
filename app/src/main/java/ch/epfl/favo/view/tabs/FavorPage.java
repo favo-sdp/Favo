20643745660425091e16d73462afd94128f779d5
mikanikos <andrea.piccione@epfl.ch>
2020-04-16 16:49:23 +0200

    Merge patch and small modifications

--- app/src/main/java/ch/epfl/favo/view/tabs/FavorPage.java
+++ app/src/main/java/ch/epfl/favo/view/tabs/FavorPage.java
@@ -156,12 +156,13 @@ public class FavorPage extends Fragment {
 
     // if returned from FavorDetail view, continue to show the search mode
 
-      // commenting for the moment because it doesn't work with androidx, will work on it in the next PR when sync with database
-//    if (!favorsFound.isEmpty()) {
-//      searchView.setIconified(false);
-//      searchView.clearFocus();
-//      setupSearchMode();
-//    }
+    // commenting for the moment because it doesn't work with androidx, will work on it in the next
+    // PR when sync with database
+    //    if (!favorsFound.isEmpty()) {
+    //      searchView.setIconified(false);
+    //      searchView.clearFocus();
+    //      setupSearchMode();
+    //    }
 
     // replacing the other two callbacks because they were buggy according to some stack overflow
     // forums
@@ -195,7 +196,8 @@ public class FavorPage extends Fragment {
           @Override
           public boolean onQueryTextChange(String newText) {
             // replace irrelevant items on listView with last query results or empty view
-            displayFavorList(new HashMap<>(), R.string.empty);
+
+            // displayFavorList(new HashMap<>(), R.string.empty);
             return false;
           }
         });
