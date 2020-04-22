43e2de1749394d9529539bdbaafe12ccee7cc3dd
rootofallevii <lastwhisper98@gmail.com>
2020-04-21 22:02:38 -0400

    Adding tests for user settings

--- app/src/main/java/ch/epfl/favo/util/UserSettings.java
+++ app/src/main/java/ch/epfl/favo/util/UserSettings.java
@@ -6,31 +6,31 @@ import static ch.epfl.favo.cache.LocalCache.getValueFromCacheBool;
 import static ch.epfl.favo.cache.LocalCache.getValueFromCacheStr;
 
 
-public class UserSettings {
+class UserSettings {
 
-  public static Boolean getPreferenceNotifNew(Context context) {
+  static Boolean getPreferenceNotifNew(Context context) {
     return getValueFromCacheBool(context, "notifications_new");
   }
 
-  public static String getNotificationRadius(Context context) {
+  static String getNotificationRadius(Context context) {
     if (getPreferenceNotifNew(context))
       return getValueFromCacheStr(context, "radius");
     return "disabled";
   }
 
-  public static Boolean getPreferenceNotifChat(Context context) {
+  static Boolean getPreferenceNotifChat(Context context) {
     return getValueFromCacheBool(context, "notifications_chat");
   }
 
-  public static Boolean getPreferenceNotifUpate(Context context) {
+  static Boolean getPreferenceNotifUpdate(Context context) {
     return getValueFromCacheBool(context, "notifications_update");
   }
 
-  public static Boolean getPreferenceNotifDelete(Context context) {
+  static Boolean getPreferenceNotifDelete(Context context) {
     return getValueFromCacheBool(context, "notifications_delete");
   }
 
-  public static Boolean getPreferenceNotifJoin(Context context) {
+  static Boolean getPreferenceNotifJoin(Context context) {
     return getValueFromCacheBool(context, "notifications_join");
   }
 
--- app/src/main/java/ch/epfl/favo/view/tabs/FragmentSettings.java
+++ app/src/main/java/ch/epfl/favo/view/tabs/FragmentSettings.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import androidx.preference.PreferenceFragmentCompat;
 
 import ch.epfl.favo.R;
-import ch.epfl.favo.cache.LocalCache;
 
 public class FragmentSettings extends PreferenceFragmentCompat {
 
@@ -20,9 +19,10 @@ public class FragmentSettings extends PreferenceFragmentCompat {
   @Override
   public void onStart() {
     super.onStart();
-    LocalCache.storeKeyValue(getContext(), "key1", "value1");
-    Log.d(TAG, "onCreatePreferences: stored key1 value1 ");
-    String val = LocalCache.getValueFromCacheStr(getContext(), "radius");
-    Log.d(TAG, String.format("onCreatePreferences: retrieved %s", val));
+    //todo: use this to load preferences if necessary
+//    LocalCache.storeKeyValue(getContext(), "key1", "value1");
+//    Log.d(TAG, "onCreatePreferences: stored key1 value1 ");
+//    String val = LocalCache.getValueFromCacheStr(getContext(), "radius");
+//    Log.d(TAG, String.format("onCreatePreferences: retrieved %s", val));
   }
 }
