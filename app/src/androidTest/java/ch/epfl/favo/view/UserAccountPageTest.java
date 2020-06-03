c947fad0e7710ee5350dafb7d1c16d14bb82f78d
Alex <alexrosu97@gmail.com>
2020-06-03 12:36:32 -0400

    picture upload unit tests

--- app/src/androidTest/java/ch/epfl/favo/view/UserAccountPageTest.java
+++ app/src/androidTest/java/ch/epfl/favo/view/UserAccountPageTest.java
@@ -1,5 +1,12 @@
 package ch.epfl.favo.view;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
@@ -11,15 +18,22 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.Objects;
+
 import ch.epfl.favo.FakeFirebaseUser;
 import ch.epfl.favo.FakeUserUtil;
 import ch.epfl.favo.FakeViewModel;
+import ch.epfl.favo.MainActivity;
 import ch.epfl.favo.R;
 import ch.epfl.favo.TestConstants;
 import ch.epfl.favo.auth.SignInActivity;
+import ch.epfl.favo.cache.CacheUtil;
 import ch.epfl.favo.user.User;
 import ch.epfl.favo.util.DependencyFactory;
+import ch.epfl.favo.view.tabs.UserAccountPage;
+import ch.epfl.favo.view.tabs.addFavor.FavorEditingView;
 
+import static android.app.Activity.RESULT_OK;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -27,10 +41,12 @@ import static androidx.test.espresso.matcher.RootMatchers.isDialog;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.internal.runner.junit4.statement.UiThreadStatement.runOnUiThread;
 import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 import static ch.epfl.favo.TestConstants.EMAIL;
 import static ch.epfl.favo.TestConstants.NAME;
 import static ch.epfl.favo.TestConstants.PHOTO_URI;
+import static ch.epfl.favo.TestConstants.PROFILE_PICTURE_ID;
 import static ch.epfl.favo.TestConstants.PROVIDER;
 import static org.hamcrest.core.StringEndsWith.endsWith;
 
@@ -40,6 +56,17 @@ public class UserAccountPageTest {
   @Rule
   public final ActivityTestRule<SignInActivity> mActivityRule =
       new ActivityTestRule<>(SignInActivity.class, true, false);
+
+  @Rule
+  public final ActivityTestRule<MainActivity> mainActivityTestRule =
+    new ActivityTestRule<MainActivity>(MainActivity.class) {
+      @Override
+      protected void beforeActivityLaunched() {
+        DependencyFactory.setCurrentFirebaseUser(
+          new FakeFirebaseUser(NAME, EMAIL, PHOTO_URI, PROVIDER));
+        DependencyFactory.setCurrentViewModelClass(FakeViewModel.class);
+      }
+    };
 
   @Rule
   public GrantPermissionRule permissionRule =
@@ -195,5 +222,67 @@ public class UserAccountPageTest {
     DependencyFactory.setCurrentFirebaseUser(null);
     onView(withId(android.R.id.button1)).perform(click());
     onView(withId(R.id.edit_profile)).perform(ViewActions.scrollTo()).check(matches(isDisplayed()));
+  }
+
+  public UserAccountPage launchFragment() throws Throwable {
+    MainActivity activity = mainActivityTestRule.getActivity();
+    NavController navController = Navigation.findNavController(activity, R.id.nav_host_fragment);
+
+    runOnUiThread(() -> navController.navigate(R.id.nav_account));
+
+    Fragment navHostFragment =
+      activity.getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+    getInstrumentation().waitForIdleSync();
+    return (UserAccountPage) navHostFragment.getChildFragmentManager().getFragments().get(0);
+  }
+
+  @Test
+  public void addPictureWorks() throws Throwable {
+    mActivityRule.launchActivity(null);
+    UserAccountPage currentFragment = launchFragment();
+    navigateToAccountTab();
+    getInstrumentation().waitForIdleSync();
+    onView(withId(R.id.edit_profile)).perform(ViewActions.scrollTo()).perform(click());
+    // inject picture
+    Bitmap bm = Bitmap.createBitmap(200, 100, Bitmap.Config.RGB_565);
+    Intent intent = new Intent();
+    intent.putExtra(FavorEditingView.CAMERA_DATA_KEY, bm);
+    runOnUiThread(() -> currentFragment.onActivityResult(2, RESULT_OK, intent));
+
+    getInstrumentation().waitForIdleSync();
+
+    onView(withId(R.id.new_profile_picture)).check(matches(isDisplayed()));
+  }
+
+  @Test
+  public void loadSavedPicture() throws Throwable {
+    mActivityRule.launchActivity(null);
+    UserAccountPage currentFragment = launchFragment();
+    navigateToAccountTab();
+    getInstrumentation().waitForIdleSync();
+    onView(withId(R.id.edit_profile)).perform(ViewActions.scrollTo()).perform(click());
+    getInstrumentation().waitForIdleSync();
+    // inject picture
+    Bitmap bm = Bitmap.createBitmap(200, 100, Bitmap.Config.RGB_565);
+    Uri filePath =
+      CacheUtil.getInstance()
+        .saveToInternalStorage(
+          Objects.requireNonNull(currentFragment.getContext()), bm, PROFILE_PICTURE_ID, 0);
+    getInstrumentation().waitForIdleSync();
+    Bitmap actual =
+      CacheUtil.getInstance()
+        .loadFromInternalStorage(
+          Objects.requireNonNull(currentFragment.getContext()).getFilesDir().getAbsolutePath()
+            + "/"
+            + PROFILE_PICTURE_ID
+            + "/",
+          0)
+        .get();
+    assert (actual.sameAs(bm));
+    Intent intent = new Intent();
+    intent.setData(filePath);
+    runOnUiThread(() -> currentFragment.onActivityResult(1, RESULT_OK, intent));
+    getInstrumentation().waitForIdleSync();
+    onView(withId(R.id.new_profile_picture)).check(matches(isDisplayed()));
   }
 }
